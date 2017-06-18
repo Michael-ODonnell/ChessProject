a6a513e5380744c1f94e863c2ca0804428a18b5c
Michael O'Donnell <development@roadtodawn.co.uk>
2017-06-18 20:53:43 +0100

    Piece Factory Failing Tests.

---
+++ ChessProject-Csharp/Chess/Pieces/PieceFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gfi.Hiring {
+    internal class PieceFactory {
+
+        public IChessPiece CreatePiece(PieceType type, PieceColor color)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ChessProject-Csharp/Chess/Tests/PieceFactory_Tests.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gfi.Hiring {
+    internal class PieceFactory_Tests {
+
+        protected PieceFactory _pieceFactory;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _pieceFactory = new PieceFactory();
+        }
+
+        [Test]
+        public void TestPawnCreationIsNotNull()
+        {
+            IChessPiece piece = _pieceFactory.CreatePiece(PieceType.Pawn, PieceColor.Black);
+            Assert.IsNotNull(piece);
+        }
+        [Test]
+        public void TestPawnImplementsIPawn()
+        {
+            IChessPiece piece = _pieceFactory.CreatePiece(PieceType.Pawn, PieceColor.Black);
+            Assert.IsNotNull(piece as IPawn);
+        }
+        [Test]
+        public void TestPawnTypeCorrectlyAssigned()
+        {
+            IChessPiece piece = _pieceFactory.CreatePiece(PieceType.Pawn, PieceColor.Black);
+            Assert.AreEqual(PieceType.Pawn, piece.Type);
+        }
+        [Test]
+        public void TestBlackPawnColorCorrectlyAssigned()
+        {
+            IChessPiece piece = _pieceFactory.CreatePiece(PieceType.Pawn, PieceColor.Black);
+            Assert.AreEqual(PieceColor.Black, piece.Color);
+        }
+        [Test]
+        public void TestWhitePawnColorCorrectlyAssigned()
+        {
+            IChessPiece piece = _pieceFactory.CreatePiece(PieceType.Pawn, PieceColor.White);
+            Assert.AreEqual(PieceColor.White, piece.Color);
+        }
+    }
+}
