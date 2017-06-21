2e32e9b4648177bf10d5cf0bdd22e8a2ab5580cf
Michael O'Donnell <development@roadtodawn.co.uk>
2017-06-21 20:45:03 +0100

    Implemented MustMoveInStraightLineRule

--- ChessProject-Csharp/Chess/Rules/GeneralRules/MustMoveInStraightLineRule.cs
+++ ChessProject-Csharp/Chess/Rules/GeneralRules/MustMoveInStraightLineRule.cs
@@ -2,13 +2,34 @@
 
 namespace Gfi.Hiring {
     /// <summary>
-    /// Checks that the move endpoint is on the board
+    /// Checks that the move endpoint is on one of the four pricipal axis x, y, x=y. x=-y of the starting point
     /// </summary>
     class MustMoveInStraightLineRule : IRule {
         
         public bool IsMoveValid(IChessBoard board, Move move)
         {
-            throw new NotImplementedException();
+            // y axis
+            if(move.StartingX == move.EndingX)  
+            {
+                return true;
+            }
+            // x axis
+            if (move.StartingY == move.EndingY)
+            {
+                return true;
+            }
+            // x=y axis
+            if (move.EndingX - move.StartingX == move.EndingY - move.StartingY)
+            {
+                return true;
+            }
+            // x=-y axis
+            if (move.EndingX - move.StartingX == move.StartingY - move.EndingY)
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
